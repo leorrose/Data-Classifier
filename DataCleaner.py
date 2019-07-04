1370fdc66c797d88d14e9a0f102ca51bf4cb007b
leorrose <leor.rose@gmail.com>
2019-07-04 14:34:12 +0300

    starting

---
+++ DataCleaner.py
@@ -0,0 +1,12 @@
+class Cleaner:
+    def __init__(self):
+        pass
+
+    def fillMisingValues(self):
+        pass
+
+    def removeRows(self):
+        pass
+
+    def removeColumns(self):
+        pass
