7232705fdfa0f9edf78bbe262adde012d9513eac
leorrose <leor.rose@gmail.com>
2019-07-09 20:11:50 +0300

    discretization update

--- DataDiscretization.py
+++ DataDiscretization.py
@@ -1,30 +1,171 @@
+from MiningCalculations import MiningCalculator
+
+
 class Discretization:
     def __init__(self):
+        self.miningCalculator = MiningCalculator()
+
+    def discretizationData(self, trainData, testData, structure, numOfBins, typeOfDiscretization):
+        """
+        method to apply discretization on each numeric column in data
+        Attributes:
+            trainData(list) : list of lines in test data set each element is a list
+            testData(list) : list of lines in test data set each element is a list
+            structure(dict): the structure of data set returns {} if data set is empty, each element is
+                            columnName : {'index': index , 'values': [values]} or
+                            columnName : {'index': index , 'values': ["Numeric"]
+            numOfBins(int): number of bins for discretization
+            typeOfDiscretization(string): what method of discretization if input does not fit a method entropy based discretization
+            will be applied
+        """
+        for columnName, value in structure.items():
+            if value["values"] == ['Numeric']:
+                colIndex = value['index']
+                bins = []
+                self.sortDataByAscendingOrderOFValuesInColumn(trainData, colIndex)
+                if typeOfDiscretization.upper() == "EQUALWIDTH":
+                    bins = self.createBinsByEqualWidth(trainData, colIndex, numOfBins)
+                elif typeOfDiscretization.upper() == "EQUALDEPTH":
+                    bins = self.createBinsByEqualDepth(trainData, colIndex, numOfBins)
+                elif typeOfDiscretization.upper() == "GINI":
+                    bins = self.createBinsByGiniIndex(trainData, colIndex, numOfBins)
+                elif typeOfDiscretization.upper() == "CLUSTERING":
+                    bins = self.createBinsByClustering(trainData, colIndex, numOfBins)
+                elif typeOfDiscretization.upper() == "ENTROPY":
+                    bins = self.createBinsByEntropy(trainData, structure, columnName, numOfBins)
+                else:
+                    bins = self.createBinsByEntropy(trainData, structure, columnName, numOfBins)
+                self.discretizationOFDataByColumn(trainData, colIndex, bins)
+                self.discretizationOFDataByColumn(testData, colIndex, bins)
+
+    def discretizationOFDataByColumn(self, data, colIndex, bins):
+        """
+        method to apply discretization on column in data
+        Attributes:
+            data(list) : list of lines in data set each element is a list
+            colIndex(int): the index of column to apply discretization
+            bins(dict): a dict with string representations of the bin and its value is a function to check if value belongs to bin
+            example - {"value<X" : lambda x: x<x...}
+        """
+        for row in data:
+            for bin, checkBinFunc in bins.items():
+                if checkBinFunc(float(row[colIndex])):
+                    row[colIndex] = bin
+                    break
+
+    def sortDataByAscendingOrderOFValuesInColumn(self, data, colIndex):
+        """
+        method to sort data by ascending values in column
+        Attributes:
+            data(list) : list of lines in data set each element is a list
+            colIndex(int): the index of column to apply discretization
+        """
+        def sortFunc(x):
+            return float(x[colIndex])
+        data.sort(key=sortFunc)
+
+    def createBinsByEqualWidth(self, data, colIndex, numOfBins):
+        """
+        method to create a bins dict by Equal Width technique each key is a string representations of the bin and its value is a function to check
+        if some value belongs to the bin example {"value<X" : lambda x: x<x...}
+        Attributes:
+            data(list) : list of lines in data set each element is a list
+            colIndex(int): the index of column to create bins from
+            numOfBins(int): number of bins to create
+        Returns:
+            dict: bins dict by Equal Width technique each key is a string representations of the bin and its value is a function to check
+            if some value belongs to the bin example {"value<X" : lambda x: x<x...}
+            Attributes:
+        """
+        colData = list(map(lambda x: float(x[colIndex]), data))
+        minVal, maxVal = min(colData), max(colData)
+        width = round(((maxVal - minVal) / numOfBins), 3)
+        bins = {"value<="+str(width): lambda x: x < width}
+        for i in range(1, numOfBins-1):
+            bins[str(width) + '<value<=' + str(width+width)] = (lambda x:  width < x <= width + width)
+            width = width + width
+        bins["value>" + str(width)] = (lambda x: x > width)
+        return bins
+
+    def createBinsByEqualDepth(self, data, colIndex, numOfBins):
+        """
+        method to create a bins dict by Equal Depth technique each key is a string representations of the bin and its value is a function to check
+        if some value belongs to the bin example {"value<X" : lambda x: x<x...}
+        Attributes:
+            data(list) : list of lines in data set each element is a list
+            colIndex(int): the index of column to create bins from
+            numOfBins(int): number of bins to create
+        Returns:
+            dict: bins dict by Equal Depth technique each key is a string representations of the bin and its value is a function to check
+            if some value belongs to the bin example {"value<X" : lambda x: x<x...}
+            Attributes:
+        """
+        colData = list(map(lambda x: float(x[colIndex]), data))
+        Depth, splittedData, index = int(((len(colData) / numOfBins) + 1)), [], 0
+        for i in range(0, numOfBins):
+            splittedData, index, Depth = splittedData + [colData[index:Depth]], Depth, Depth + Depth
+        bins = {"value<=" + str(max(splittedData[0])): lambda x: x <= max(splittedData[0])}
+        index = 1
+        while index < numOfBins-1:
+            bins[str(max(splittedData[index-1])) + '<value<=' + str(max(splittedData[index]))] = \
+                (lambda x: max(splittedData[index-1]) < x <= max(splittedData[index]))
+            index += 1
+        bins["value>" + str(max(splittedData[index-1]))] = (lambda x: x > max(splittedData[index-1]))
+        return bins
+
+    def createBinsByEntropy(self, data, structure, colName, numOfBins):
+        """
+        method to create a bins dict by Entropy technique each key is a string representations of the bin and its value is a function to check
+        if some value belongs to the bin example {"value<X" : lambda x: x<x...}
+        Attributes:
+            data(list) : list of lines in data set each element is a list
+            structure(dict): the structure of data set returns {} if data set is empty, each element is
+                            columnName : {'index': index , 'values': [values]} or
+                            columnName : {'index': index , 'values': ["Numeric"]
+            colName(int): the name of column to create bins from
+            numOfBins(int): number of bins to create
+        Returns:
+            dict: bins dict by Entropy technique each key is a string representations of the bin and its value is a function to check
+            if some value belongs to the bin example {"value<X" : lambda x: x<x...}
+            Attributes:
+        """
+        splits = self.miningCalculator.getBestSplitsInDataByInfoGainInto(data, structure, colName, numOfBins-1)
+        splits.sort()
+        bins = {"value<="+str(splits[0]): lambda x: x < splits[0]}
+        for i in range(1, numOfBins-1):
+            bins[str(splits[i-1]) + '<value<=' + str(splits[i])] = (lambda x:  splits[i-1] < x <= splits[i])
+        bins["value>" + str(splits[len(splits)-1])] = (lambda x: x > splits[len(splits)-1])
+        return bins
+
+    def createBinsByGiniIndex(self, data, colIndex, numOfBins):
+        """
+        method to create a bins dict by Gini Index technique each key is a string representations of the bin and its value is a function to check
+        if some value belongs to the bin example {"value<X" : lambda x: x<x...}
+        Attributes:
+            data(list) : list of lines in data set each element is a list
+            colIndex(int): the index of column to create bins from
+            numOfBins(int): number of bins to create
+        Returns:
+            dict: bins dict by Gini Index technique each key is a string representations of the bin and its value is a function to check
+            if some value belongs to the bin example {"value<X" : lambda x: x<x...}
+            Attributes:
+        """
         pass
 
-    def DiscretizationTrainingSet(self, numOfBins):
+    def createBinsByClustering(self, data, colIndex, numOfBins):
+        """
+        method to create a bins dict by Clustering technique each key is a string representations of the bin and its value is a function to check
+        if some value belongs to the bin example {"value<X" : lambda x: x<x...}
+        Attributes:
+            data(list) : list of lines in data set each element is a list
+            colIndex(int): the index of column to create bins from
+            numOfBins(int): number of bins to create
+        Returns:
+            dict: bins dict by Clustering technique each key is a string representations of the bin and its value is a function to check
+            if some value belongs to the bin example {"value<X" : lambda x: x<x...}
+            Attributes:
+        """
         pass
 
-    def DiscretizationTestSet(self, numOfBins):
-        pass
 
-    def createBins(self):
-        pass
 
-    def createBinsByEqualWidth(self):
-        pass
-
-    def createBinsByEqualDepth(self):
-        pass
-
-    def CreateBinsByEntropy(self):
-        pass
-
-    def createBinsByGiniIndex(self):
-        pass
-
-    def createBinsByClustering(self):
-        pass
-
-    def sortDataByAscendingOrderOFValuesInColumn(self):
-        pass
--- MiningCalculations.py
+++ MiningCalculations.py
@@ -6,6 +6,16 @@ class MiningCalculator:
         pass
 
     def calcClassEntropy(self, data, structure):
+        """
+        method to calculate entropy of class in data set
+        Attributes:
+            data(list) : list of lines in files each element is a list
+            structure(dict): the structure of data set returns {} if data set is empty, each element is
+                            columnName : {'index': index , 'values': [values]} or
+                            columnName : {'index': index , 'values': ["Numeric"]
+        Returns:
+            float: the entropy of class in data set
+        """
         classIndex, entropy = structure['class']['index'], 0
         for value in structure['class']['values']:
             newData = list(filter(lambda y: y[classIndex] == value, data))
@@ -14,6 +24,17 @@ class MiningCalculator:
         return round(entropy, 3)
 
     def calcClassEntropyByColumn(self, data, structure, colName):
+        """
+        method to calculate entropy of class in data set if we split it by column
+        Attributes:
+            data(list) : list of lines in files each element is a list
+            structure(dict): the structure of data set returns {} if data set is empty, each element is
+                            columnName : {'index': index , 'values': [values]} or
+                            columnName : {'index': index , 'values': ["Numeric"]
+            colName(String): the name of column to split data
+        Returns:
+            float: the entropy of splitting data by column
+        """
         colIndex, entropy = structure[colName]['index'], 0
         for colValue in structure[colName]['values']:
             newData = list(filter(lambda y: y[colIndex] == colValue, data))
@@ -22,6 +43,18 @@ class MiningCalculator:
         return round(entropy, 3)
 
     def calcClassEntropyBySplit(self, data, structure, colName, splitVal):
+        """
+        method to calculate entropy of class in data set if we split it by a numeric split value
+        Attributes:
+            data(list) : list of lines in files each element is a list
+            structure(dict): the structure of data set returns {} if data set is empty, each element is
+                            columnName : {'index': index , 'values': [values]} or
+                            columnName : {'index': index , 'values': ["Numeric"]
+            colName(String): the name of column to split data
+            splitVal(float): the number to split data
+        Returns:
+            float: the entropy of splitting data by numeric split value
+        """
         colIndex, entropy = structure[colName]['index'], 0
         newDataBellowSplit = list(filter(lambda y: float(y[colIndex]) <= splitVal, data))
         newDataAboveSplit = list(filter(lambda y: float(y[colIndex]) > splitVal, data))
@@ -32,16 +65,49 @@ class MiningCalculator:
         return round(entropy, 3)
 
     def calcInfoGainByColumn(self, data, structure, colName):
+        """
+        method to calculate info-gain of splitting data set by numeric split value
+        Attributes:
+            data(list) : list of lines in files each element is a list
+            structure(dict): the structure of data set returns {} if data set is empty, each element is
+                            columnName : {'index': index , 'values': [values]} or
+                            columnName : {'index': index , 'values': ["Numeric"]
+            colName(String): the name of column to split data
+        Returns:
+            float: the info-gain of class in data set after splitting data by column
+        """
         result = self.calcClassEntropy(data, structure) - self.calcClassEntropyByColumn(data, structure, colName)
         result = 0 if result < 0 else result
         return round(result, 3)
 
     def calcInfoGainBySplit(self, data, structure, colName, splitVal):
+        """
+        method to calculate info-gain of splitting data by a a
+        Attributes:
+            data(list) : list of lines in files each element is a list
+            structure(dict): the structure of data set returns {} if data set is empty, each element is
+                            columnName : {'index': index , 'values': [values]} or
+                            columnName : {'index': index , 'values': ["Numeric"]
+            colName(String): the name of column to split data
+        Returns:
+            float: the info-gain of class in data set after splitting data by column
+        """
         result = self.calcClassEntropy(data, structure) - self.calcClassEntropyBySplit(data, structure, colName, splitVal)
         result = 0 if result < 0 else result
         return round(result, 3)
 
     def findBestSplitInDataByInfoGain(self, data, structure, colName):
+        """
+        method to find best split in the data by info-gain
+        Attributes:
+            data(list) : list of lines in files each element is a list
+            structure(dict): the structure of data set returns {} if data set is empty, each element is
+                            columnName : {'index': index , 'values': [values]} or
+                            columnName : {'index': index , 'values': ["Numeric"]
+            colName(String): the name of column to find splits of data
+        Returns:
+            list: best split in the data by info-gain and its info gain value example - [split, infoGain]
+        """
         colIndex, maxInfoGain, bestSplit = structure[colName]['index'], 0, []
         for i in range(0, len(data)-1):
             split = (float(data[i][colIndex]) + float(data[i+1][colIndex])) / 2
@@ -52,6 +118,21 @@ class MiningCalculator:
         return bestSplit
 
     def fillBestSplitsInDataByInfoGainIntoSplitList(self, data, structure, colName, numOfSplits, splitsList, indexToInsert):
+        """
+        recursive method to fill a list with best splits in the data by info-gain
+        Attributes:
+            data(list) : list of lines in files each element is a list
+            structure(dict): the structure of data set returns {} if data set is empty, each element is
+                            columnName : {'index': index , 'values': [values]} or
+                            columnName : {'index': index , 'values': ["Numeric"]
+            colName(String): the name of column to find splits of data
+            numOfSplits(int): number of splits to find
+            splitsList(list): list to fill splits in it
+            indexToInsert(int): an index to insert the splits in dict to keep splits order in recursive method
+        Returns:
+            dict: dict of number of split (when it Happens) and value of list of
+            best split in the data by info-gain and its info gain value example - {split number: [[split, infoGain],[split, infoGain]]
+        """
         if len(data) <= 0 or numOfSplits <= 0:
             return []
         colIndex = structure[colName]['index']
@@ -69,6 +150,18 @@ class MiningCalculator:
             self.fillBestSplitsInDataByInfoGainIntoSplitList(newDataAboveSplit, structure, colName, numOfSplits, splitsList, indexToInsert)
 
     def getBestSplitsInDataByInfoGainInto(self, data, structure, colName, numOfSplits):
+        """
+        method to get a list with best splits in the data by info-gain
+        Attributes:
+            data(list) : list of lines in files each element is a list
+            structure(dict): the structure of data set returns {} if data set is empty, each element is
+                            columnName : {'index': index , 'values': [values]} or
+                            columnName : {'index': index , 'values': ["Numeric"]
+            colName(String): the name of column to find splits of data
+            numOfSplits(int): number of splits to find
+        Returns:
+            list: best splits in the data by info-gain ordered by best split to take, example - [splitOne, splitTwo, SplitThree...]
+        """
         splitsList, newSplitsList = {}, []
         self.fillBestSplitsInDataByInfoGainIntoSplitList(data, structure, colName, numOfSplits, splitsList, 0)
         for lists in list(splitsList.values())[1:]:
@@ -88,5 +181,17 @@ class MiningCalculator:
                     newSplitsList.append(splitTwo[0])
 
         newSplitsList.insert(0, splitsList['0'][0][0])
+        self.removeDuplicates(newSplitsList)
         return newSplitsList[0:numOfSplits]
 
+    def removeDuplicates(self, data):
+        """
+        method to remove duplicates from list
+        Attributes:
+            data(list) : list of values
+        Returns:
+            list: data with no duplicates
+        """
+        for i in range(0, len(data)):
+            if data.count(data[i]) > 1:
+                data.remove(data[i])
